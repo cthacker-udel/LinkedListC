5f25af83e20c3298355d904b4e5a9e89963e90ac
cthacker-udel <cthacker@udel.edu>
2021-05-04 00:09:41 -0400

    Added print nodes of double linked list functionality

--- doubleLinkedList/linkedlistdouble.c
+++ doubleLinkedList/linkedlistdouble.c
@@ -11,3 +11,57 @@ struct node{
     struct node *previous;
 
 };
+
+
+struct node *HEAD = NULL;
+struct node *TEMP = NULL;
+
+
+void printNodes(){
+
+    printf("\nThe number of nodes is : %d\n",numberOfNodes());
+
+}
+
+
+int numberOfNodes(){
+
+    int count = 0;
+    struct node *currNode = HEAD;
+    while(currNode != NULL){
+        currNode = currNode->next;
+        count++;
+    }
+    return count;
+
+}
+
+
+void printMenu(){
+
+    printf("\n~=~=~=MENU~=~=~=\n0)Print nodes\n1)Exit Program");
+
+}
+
+
+int main(void){
+
+    int choice;
+    int nodeValue;
+
+    while(1){
+
+        printMenu();
+        scanf("%d",&choice);
+        switch(choice){
+
+            case 0:
+                print
+
+        }
+
+
+    }
+
+
+}
