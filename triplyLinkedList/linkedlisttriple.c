633bd8903cb6e5b9677881dd11ff5bc75f76f108
cthacker-udel <cthacker@udel.edu>
2021-05-07 01:59:43 -0400

    Added add node functionality for triply linked list

--- triplyLinkedList/linkedlisttriple.c
+++ triplyLinkedList/linkedlisttriple.c
@@ -15,6 +15,31 @@ struct node *TAIL = NULL;
 
 
 
+
+void addNode(int value){
+
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->value = value;
+    if(HEAD == NULL){
+        newNode->next = NULL;
+        newNode->previous = NULL;
+        HEAD = newNode;
+        TAIL = newNode;
+    }
+    else{
+
+        struct node *currNode = (struct node *)malloc(sizeof(struct node));
+        while(currNode->next != NULL){
+            currNode = currNode->next;
+        }
+        currNode->next = newNode;
+        newNode->next = NULL;
+        newNode->previous = currNode;
+        TAIL = newNode;
+    }
+
+} 
+
 void printMenu(){
 
     printf("\n~=~=~=MENU~=~=~=\n");
